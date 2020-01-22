39fd5c6b66aacc9253f00afe28f43381df5f7695
Emily Kwak <emilykwak1@gmail.com>
2020-01-22 13:22:37 -0800

    handle duplicate entries

--- data/index.js
+++ data/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 // const seed = require('./seed.js');
 
-mongoose.connect('mongodb://localhost:27017/earnings', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/earnings', { useNewUrlParser: true, useUnifiedTopology: true });
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -9,13 +9,13 @@ db.once('open', () => console.log('db connected'));
 
 
 const earningsSchema = new mongoose.Schema({
-  ticker: String,
-  name: String,
+  ticker: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   earnings: [
     {
       dateTime: Date,
       revenue: Number,
-      estRevenue: Number 
+      estRevenue: Number
     }
   ]
 });
