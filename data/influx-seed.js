26aa9d5ed86e0fd6535d750076dd4893d6748435
Emily Kwak <emilykwak1@gmail.com>
2020-02-03 20:02:09 -0800

    [WIP] - influx-seed

---
+++ data/influx-seed.js
@@ -0,0 +1,25 @@
+const influx = require('./influxdb/index.js');
+const earnings = require('./earnings.js');
+const tickers = require('./tickers.js');
+
+const start = () => {
+  let tickerList = tickers.createTickers().map(ticker => {
+    return {
+      measurement: 'earning',
+      tags: {
+        ticker: ticker,
+        name: earnings.generateName(),
+      },
+      fields: { earnings: earnings.generateEarningsList() },
+    };
+  });
+  return tickerList;
+}
+
+influx.writePoints(start(), {
+  database: 'earnings',
+})
+  .then(() => console.log('Data Seeded!'))
+  .catch(err => {
+    console.error(`Error saving data to InfluxDB! ${err}`)
+  });
