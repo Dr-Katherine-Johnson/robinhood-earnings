94a9b0c41fc6a02c056a68b5d013c2267f5dacbc
Emily Kwak <emilykwak1@gmail.com>
2020-02-24 17:09:34 -0800

    rename file to use seed.js

--- data/seed.js
+++ data/seed.js
@@ -1,11 +1,10 @@
+const { db, pgp } = require('./postgres/index.js');
 const earnings = require('./earnings.js');
 const tickers = require('./tickers.js');
-const db = require('./index.js');
 
-// needs to create the array of all the tickers, and add the additional price data
+const cs = new pgp.helpers.ColumnSet(['ticker', 'name', 'earnings'], { table: 'tickers' });
 
 const start = () => {
-  let date = ['Q2 2018', 'Q3 2018', 'Q4 2018', 'Q1 2019', 'Q2 2019', 'Q3 2019', 'Q4 2019'];
   let tickerList = tickers.createTickers().map(ticker => {
     return {
       ticker: ticker,
@@ -16,28 +15,26 @@ const start = () => {
   return tickerList;
 }
 
-// able to save data by chunks
-// need to handle error
-async function seedDatabase() {
-  await db.Earnings.deleteMany({}, () => {
-    console.log('Remove Earnings')
-  })
-
-  let total = 50000
+async function seedDB() {
+  let total = 1000000
   let count = 0;
   let chunk = start().length;
 
   while (count < total) {
     count += chunk;
-    await db.Earnings.insertMany(start());
+    await db.none(pgp.helpers.insert(start(), cs))
+      .then(() => {
+        console.log('Success')
+      })
+      .catch(err => {
+        console.log(`Error: ${err}`)
+      });
     console.log(count);
   }
 
   if (count >= total) {
-    console.log('Earnings seeded to database!')
+    console.log('Tickers seeded to database!')
   }
 }
 
-seedDatabase();
-
-// module.exports.seedDatabase();
+seedDB();
