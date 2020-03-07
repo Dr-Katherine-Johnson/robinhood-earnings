8891e547849c59500b57a536ac52b066d2b2e76b
Emily Kwak <emilykwak1@gmail.com>
2020-03-06 20:02:44 -0800

    add random ids

--- k6Script.js
+++ k6Script.js
@@ -1,8 +1,20 @@
 import http from "k6/http";
 import { check } from "k6";
 
+export let options = {
+  vus: 30,
+  duration: '30s',
+};
+
 export default function () {
-  let res = http.get("http://localhost:3007/earnings/P2M1QSWZ2MD2BR");
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  let id = getRandomInt(1, 1000000)
+  let res = http.get(`http://localhost:3007/earnings/${id}`);
   check(res, {
     "is status 200": (r) => r.status === 200
   });
